d462daedec6edb8649c999cc0337929564e359fa
Saját Neved <email@pelda.hu>
2025-10-19 11:30:44 +0200

    setting up the window with conf

--- ablak.py
+++ ablak.py
@@ -1,3 +1,17 @@
+import os
+os.environ['KIVY_NO_CONSOLELOG'] ='1'
+
+cwd =os.getcwd()
+os.environ['KIVY_HOME'] = cwd + '/conf'
+
+# from kivy.config import Config
+# Config.set('graphics', 'widht', '1280')
+# Config.set('graphics', 'height', '720')
+# Config.set('graphics', 'resizable', '0') #1 True >> resizable#
+# Config.set('graphics', 'borderless', '0')
+#Config.write() => save this attributes int the .kivy config ini file
+
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 
